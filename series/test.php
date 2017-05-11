58d18ba613021862fb367800df7c409c8e52c674
amitoshseu <amitosh_cse_seu@yahoo.com>
2017-05-11 17:12:17 +0600

    modified test file

--- series/test.php
+++ series/test.php
@@ -1,45 +1,7 @@
-<?php
-// Declare the class
-class Car {
- 
-  // properties
-  public $comp; 
-  public $color = 'beige';
-  public $hasSunRoof = true;
- 
-   // method that says hello
-  public function hello()
-  {
-    return "beep";
-  }
-}
-	 
-	// Create an instance
-	$bmw = new Car ();
-	$mercedes = new Car ();
-	 
-	// Get the values
-	echo $bmw -> color; // beige
-	echo "<br />";
-	echo $mercedes -> color; // beige
-	echo "<hr />";
-	 
-	// Set the values
-	$bmw -> color = 'blue';
-	$bmw -> comp = "BMW";
-	$mercedes -> comp = "Mercedes Benz";
-	 
-	// Get the values again
-	echo $bmw -> color; // blue
-	echo "<br />";
-	echo $mercedes -> color; // beige
-	echo "<br />";
-	echo $bmw -> comp; // BMW
-	echo "<br />";
-	echo $mercedes -> comp; // Mercedes Benz
-	echo "<hr />";
-	 
-	// Use the methods to get a beep
-	echo $bmw -> hello(); // beep
-	echo "<br />";
-	echo $mercedes -> hello(); // beep
+<form action="another.php" method="post">
+	Name: <input type="text" name="name"><br>
+	Email:<input type="text" name="email"><br>
+	Password:<input type="password" name="pass"><br>
+	<input type="submit" name=" SUBMIT ">
+</form>
+
