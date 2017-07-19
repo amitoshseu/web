cda549d0036dc6bbe1f16563864baf29f56b07dc
amitoshseu <amitosh_cse_seu@yahoo.com>
2017-07-19 11:50:08 +0600

    added staticClass

---
+++ php_oop/_4staticMethod.php
@@ -0,0 +1,12 @@
+<?php 
+	class Math 
+	{
+		public static function say_hi()
+		{
+			echo "This Math class says 'Hello there'. <br>";
+		}
+	}
+
+	echo "Using the Math class... <br>";
+	Math::say_hi();	
+?>
